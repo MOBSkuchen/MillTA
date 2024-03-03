fd585f7a3d0ffc45bd8c85060cc716c2a1b117ba
MOBSkuchen <71943003+MOBSkuchen@users.noreply.github.com>
2024-03-03 19:44:20 +0100

    Fix

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
+import sys
 from argument_parser import ArgumentParser, Argument, Flag
 from os_flags import set_flag
 from errors import sys_exit
 
-__version = "0.0.1"
+__version__ = "0.0.1"
+EX_FILE_NAME = sys.argv.pop(0)
 PROG_NAME = "MillTA"
 set_flag("PROG_NAME", PROG_NAME)
-set_flag("VERSION", __version)
+set_flag("VERSION", __version__)
 ap = None
 
 
