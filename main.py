209b1c1754e1761b13da81346615905f558106e2
MOBSkuchen <71943003+MOBSkuchen@users.noreply.github.com>
2024-03-04 17:07:51 +0100

    Create a parser command

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import io
 import sys
 from argument_parser import ArgumentParser, Argument, Flag
-from os_flags import set_flag, set_bool_flag
+from os_flags import set_flag
 from errors import sys_exit, SuppliedError, warning
 from lexer import PLexer
 from rich import get_console
+from parser import PParser
 
 console = get_console()
 __version__ = "0.0.1"
@@ -25,13 +26,30 @@ def try_get_file(filepath) -> io.FileIO:
         SuppliedError(filepath)()
 
 
-def lex_f(filepath):
-    f = try_get_file(filepath)
+def tokenize(f: io.FileIO):
     lexer = PLexer(f)
     tokens = lexer.tokenize(f.read())
-    for token in tokens:
+    return tokens
+
+
+def parse(f: io.FileIO, tokens):
+    parser = PParser(f)
+    ast = parser.do_parse(tokens)
+    return ast
+
+
+def lex_f(filepath):
+    f = try_get_file(filepath)
+    for token in tokenize(f):
         console.print(f"LINE {token.lineno - 1} [{token.index}-{token.end}] | [bold]{token.type}[/] : {token.value}")
     f.close()
+
+
+def parse_f(filepath):
+    f = try_get_file(filepath)
+    tokens = tokenize(f)
+    ast = parse(f, tokens)
+    console.print(ast)
 
 
 def main():
@@ -40,6 +58,7 @@ def main():
         .add_help() \
         .add_version() \
         .add(Argument("lex", ["filepath"], lex_f, "Lexes a file")) \
+        .add(Argument("parse", ["filepath"], parse_f, "Parses a file")) \
         .add(Flag("disable-alerts", "a", None, "DISABLE_ALERTS", "Disables alters (warnings)")) \
         .parse()
 
